75bbef5c7cd5b53896dfea491f8bd784434a05c5
PraneethReddyDarapu <pdarapu@albany.edu>
2023-02-20 20:27:24 -0500

    firebase and blog changes

---
+++ where_next/src/Components/ImageUpload/imageUpload.js
@@ -0,0 +1,61 @@
+import "./imageUpload.css";
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "./../../../src/firebase";
+import { v4 } from "uuid";
+
+function Imageup() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div className="ImageUploadArea">
+    <div className="fileupload">
+        <span>
+    <h2>You can upload travel pictures here ...</h2>
+      <input
+        type="file" class="btn btn-light"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      </span>
+      </div>
+      <button class="btn btn-dark" onClick={uploadFile}> Upload Image</button>
+      <span>
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
+      </span>
+    </div>
+  );
+}
+
+export default Imageup;
--- where_next/src/pages/Blog/Blog.js
+++ where_next/src/pages/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import Imageup from '../../Components/ImageUpload/imageUpload';
 const Blog = () => {
   return (
     <div className="container">
+      <Imageup/>
     
         <h1 className="text-center" style={{paddingTop: "30%"}}>
           Services
