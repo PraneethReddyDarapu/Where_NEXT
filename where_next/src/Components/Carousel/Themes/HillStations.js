441239530529642342e3fd498b1dc120e5f89399
Deepshika6 <64570889+Deepshika6@users.noreply.github.com>
2023-02-26 14:30:20 -0500

    Commit the additional pages

---
+++ where_next/src/Components/Carousel/Themes/HillStations.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import SimpleSliderInternal from "../SimpleSlider/SimpleSlider_Internal"
+const HillStations = () => {
+  return (
+    <div className="container">
+      <SimpleSliderInternal data={data} />
+    </div>)
+}
+export default HillStations;
+
+const data = [
+  {
+    id: 1,
+    image: "../images/TravelTheme1.jpg",
+    title:"Mountains",
+    description: "Description",
+    country:"USA"
+  },
+  {
+    id: 2,
+    image: "../images/TravelTheme2.jpg",
+    title:"Hill Stations",
+    description:"Description",
+  },
+  {
+    id: 3,
+    image: "../images/TravelTheme3.jpg",
+    title:"Deserts",
+    description:"Description",
+    country:"USA"
+  },
+  {
+    id: 4,
+    image: "../images/TravelTheme4.jpg",
+    title:"Adventure",
+    description:"Description"
+  },
+
+  {
+    id: 5,
+    image: "../images/TravelTheme5.jpg",
+    title:"Heritage",
+    description:"Description"
+  },
+  {
+    id: 6,
+    image: "../images/TravelTheme6.jpg",
+    title:"Beaches",
+    description:"Description"
+  },
+];
